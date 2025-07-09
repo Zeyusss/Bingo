2824cadd8ab86558d653f424b3185bb5d9f650a6
Zeyusss <dght839@gmail.com>
2025-07-09 04:47:53 +0300

    Seller : All Product Implmentation Done , Delete-Restore Feature.

--- apps/product-service/src/routes/product.routes.ts
+++ apps/product-service/src/routes/product.routes.ts
@@ -1,5 +1,5 @@
 import express, {Router} from "express";
-import { createDiscountCodes, deleteDiscountCode, getCategories, getDiscountCodes } from "../controllers/product.controller";
+import { createDiscountCodes, createProduct, deleteDiscountCode, deleteProduct, deleteProductImage, getCategories, getDiscountCodes, getShopProducts, restoreProduct, uploadProductImage } from "../controllers/product.controller";
 import isAuthenticated from "@packages/middleware/isAuthenticated";
 
 
@@ -10,5 +10,11 @@ router.get("/get-categories",getCategories);
 router.post("/create-discount-code",isAuthenticated,createDiscountCodes);
 router.get("/get-discount-codes",isAuthenticated,getDiscountCodes);
 router.delete("/delete-discount-code/:id",isAuthenticated,deleteDiscountCode);
+router.post("/upload-product-image",isAuthenticated,uploadProductImage);
+router.delete("/delete-product-image",isAuthenticated,deleteProductImage);
+router.post("/create-product",isAuthenticated,createProduct);
+router.get("/get-shop-products",isAuthenticated,getShopProducts);
+router.delete("/delete-product/:productId",isAuthenticated,deleteProduct);
+router.put("/restore-product/:productId",isAuthenticated,restoreProduct);
 
 export default router;
--- apps/seller-ui/next.config.js
+++ apps/seller-ui/next.config.js
@@ -11,7 +11,15 @@ const nextConfig = {
   // Use this to set Nx-specific options
   // See: https://nx.dev/recipes/next/next-config-setup
   nx: {},
-};
+  images:{
+  remotePatterns:[
+    {
+      hostname : "ik.imagekit.io",
+    }
+  ]
+}
+}
+
 
 const plugins = [
   // Add more Next.js plugins to this list if needed.
