8e4996e06b48b0cb540d75486f0dba6eb22b8d96
Zeyusss <dght839@gmail.com>
2025-07-16 23:49:00 +0300

    User UI : Some homepage Fixes

---
+++ apps/user-ui/src/shared/components/homepage/TopOffersSection.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import axiosInstance from "../../../utils/axiosInstance";
+import ProductCard from "../cards/product-card";
+
+const TopOffersSection = () => {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["top-offers"],
+    queryFn: async () => {
+      const res = await axiosInstance.get("/product/api/get-all-events?page=1&limit=10");
+      return res.data.events;
+    },
+    staleTime: 1000 * 60 * 2,
+  });
+
+  return (
+    <div className="my-8 block">
+      <h2 className="md:text-3xl text-xl font-semibold mb-4">Top Offers</h2>
+      <div className="bg-orange-50 rounded-xl shadow-inner p-6 mb-12">
+        {isLoading ? (
+          <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 2xl:grid-cols-5 gap-5">
+            {Array.from({ length: 10 }).map((_, index) => (
+              <div
+                key={index}
+                className="h-[250px] bg-gray-300 animate-pulse rounded-xl"
+              />
+            ))}
+          </div>
+        ) : isError ? (
+          <p className="text-center text-red-500">No Offers available!</p>
+        ) : data?.length ? (
+          <div className="m-auto grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 2xl:grid-cols-5 gap-5">
+            {data.map((product: any) => (
+              <ProductCard key={product.id} product={product} isEvent={!!product.starting_date} />
+            ))}
+          </div>
+        ) : (
+          <p className="text-center">No products to display.</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default TopOffersSection; 
