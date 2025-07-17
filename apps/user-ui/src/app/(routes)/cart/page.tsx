c669aeec6b7c757d9ba3265dc3e7124af58086fb
Zeyusss <dght839@gmail.com>
2025-07-17 20:56:42 +0300

    User UI :Shipping address system Fully working

--- apps/user-ui/src/app/(routes)/cart/page.tsx
+++ apps/user-ui/src/app/(routes)/cart/page.tsx
@@ -1,8 +1,10 @@
 "use client"
+import { useQuery } from '@tanstack/react-query';
 import useDeviceTracking from 'apps/user-ui/src/hooks/useDeviceTracking';
 import useLocationTracking from 'apps/user-ui/src/hooks/useLocationTracking';
 import useUser from 'apps/user-ui/src/hooks/useUser';
 import { useStore } from 'apps/user-ui/src/store';
+import axiosInstance from 'apps/user-ui/src/utils/axiosInstance';
 import { Loader2 } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -57,6 +59,22 @@ const decreaseQuantity = (id: string) => {
     }
   }, [isLoading, user, router]);
 
+  const {data:addresses = []} = useQuery<any[],Error>({
+    queryKey : ["shipping-addresses"],
+    queryFn: async()=> {
+      const res = await axiosInstance.get("/api/shipping-addresses");
+      return res.data.addresses;
+    },
+  });
+
+  useEffect(()=>{
+    if(addresses.length > 0 && !selectedAddressId){
+      const defaultAddr = addresses.find((addr)=> addr.isDefault);
+      if(defaultAddr){
+        setSelectedAddressId(defaultAddr.id);
+      }
+    }
+  },[addresses,selectedAddressId])
 
   return (
     <div className='w-full bg-white'>
@@ -217,12 +235,24 @@ const decreaseQuantity = (id: string) => {
         <h4 className='mb-[7px] font-medium text-[15px]'>
         Select Shipping Address
         </h4>
-        <select className='w-full p-2 border border-gray-200 rounded-md focus:outline-none focus:border-blue-500'
-        value={selectedAddressId}
-        onChange={(e)=> setSelectedAddressId(e.target.value)}
-        >
-        <option value="123">Home -  Sharkia - Egypt</option>
-        </select>
+        {addresses?.length !== 0 && (
+          <select 
+          className='w-full p-2 border border-gray-200 rounded-md focus:outline-none focus:border-blue-500'
+          value={selectedAddressId}
+          onChange={(e)=> setSelectedAddressId(e.target.value)}
+          >
+          {addresses?.map((address:any)=>(
+            <option key={address.id} value={address.id}>
+            {address.street} - {address.city} , {address.country}
+            </option>
+          ))}
+          </select>
+        )}
+        {addresses?.length === 0 && (
+          <p className='text-sm text-slate-800'>
+          Please add an address from profile to create an order!
+          </p>
+        )}
     </div>
         <hr className='my-4 text-slate-200' />
     <div className='mb-4'>
