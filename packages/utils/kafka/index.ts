6c30f555670bfbb58bef61e21fa0aec16da0a5ad
Zeyusss <dght839@gmail.com>
2026-06-15 20:46:18 +0300

    fix(kafka): update broker address for Kafka connectionfix(tsconfig): streamline compiler options formattingchore: remove unused files

--- packages/utils/kafka/index.ts
+++ packages/utils/kafka/index.ts
@@ -3,7 +3,7 @@ import { Kafka } from "kafkajs";
 export const kafka = new Kafka({
   clientId: "kafka-service",
   brokers: [
-    "d83ls1hto7lor6koghug.any.us-east-1.mpx.prd.cloud.redpanda.com:9092",
+    "d8o0v2p526n3gtifi6m0.any.us-east-1.mpx.prd.cloud.redpanda.com:9092",
   ],
   ssl: true,
   sasl: {
